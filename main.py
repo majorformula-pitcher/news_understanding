f70a67d8eaa35a1a3855fdea7ead11b8cd6313ce
majorformula <majorformula@gmail.com>
2026-03-20 08:58:30 +0000

    chore: configure for render.com deployment

--- main.py
+++ main.py
@@ -185,4 +185,6 @@ async def post_index(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
